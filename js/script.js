21e09ff2aafd43f4618ac0073d52f0167b8ec8fa
konradjozefczyk <konradjozefczyk@hotmail.com>
2024-08-12 11:59:21 +0200

    rozpoczęta praca

--- js/script.js
+++ js/script.js
@@ -14,15 +14,67 @@ const handleQuiz = e => {
 
 form.addEventListener('submit', handleQuiz)
 
-
 function cennik() {
-	let var1 = document.querySelector("input[name=question-1]:checked").value;
-	let var2 = document.querySelector("input[name=question-2]:checked").value;
-	let var3 = document.querySelector("input[name=question-3]:checked").value;
-	console.log(var1);
-	console.log(var2);
-	console.log(var3);
+	let var1 = document.querySelector('input[name=question-1]:checked').value
+	let var2 = document.querySelector('input[name=question-2]:checked').value
+	let var3 = document.querySelector('input[name=question-3]:checked').value
+	console.log(var1)
+	console.log(var2)
+	console.log(var3)
 
-	cena = parseInt(var1) + parseInt(var2) + parseInt(var3);
-	document.getElementById("change").innerHTML = cena;
+	cena = parseInt(var1) + parseInt(var2) + parseInt(var3)
+	document.getElementById('change').innerHTML = cena
 }
+
+window.addEventListener(
+	'scroll',
+	() => {
+		document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight))
+	},
+	false
+)
+
+function menuOpen() {
+	var menu = document.getElementById('menuitems')
+	if (menu.style.display === 'block') {
+		menu.style.display = 'none'
+	} else {
+		menu.style.display = 'block'
+	}
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const counters = document.querySelectorAll('.counter')
+
+	const options = {
+		root: null, // viewport
+		threshold: 0.1, // uruchom, gdy 10% elementu jest widoczne
+	}
+
+	const observer = new IntersectionObserver((entries, observer) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const counter = entry.target
+				const updateCount = () => {
+					const target = +counter.getAttribute('data-target')
+					const count = +counter.innerText
+					const increment = target / 500 // dostosuj, aby kontrolować prędkość
+
+					if (count < target) {
+						counter.innerText = Math.ceil(count + increment)
+						setTimeout(updateCount, 1) // opóźnienie między inkrementacjami
+					} else {
+						counter.innerText = target
+					}
+				}
+
+				updateCount()
+				observer.unobserve(counter) // przestań obserwować, gdy licznik się uruchomi
+			}
+		})
+	}, options)
+
+	counters.forEach(counter => {
+		observer.observe(counter)
+	})
+})
